eba9594243b3ef73d06f24598905311acddf3d94
Peter Mösenthin <moesenthin@sipgate.de>
2019-03-15 17:54:35 +0100

    add techs

--- src/firestore.ts
+++ src/firestore.ts
@@ -298,6 +298,72 @@ export const insertTechnologies = () => {
 		{
 			name: "CMD",
 			image: "https://ih1.redbubble.net/image.439808265.0172/flat,800x800,075,f.u2.jpg"
+		},
+		{
+			name: "Scala",
+			image: "https://www.scala-lang.org/resources/img/frontpage/scala-spiral.png"
+		},
+		{
+			name: "Kotlin",
+			image: "https://pbs.twimg.com/profile_images/699217734492647428/pCfEzr6L.png"
+		},
+		{
+			name: "Rust",
+			image:
+				"https://ih1.redbubble.net/image.331530726.0562/ap,550x550,12x12,1,transparent,t.u1.png"
+		},
+		{
+			name: "Perl",
+			image: "https://cdn.worldvectorlogo.com/logos/perl-programming-language.svg"
+		},
+		{
+			name: "Cobol",
+			image: "http://www.devsanon.com/wp-content/uploads/2016/01/cobol.png"
+		},
+		{
+			name: "Assembler",
+			image: "https://f4.bcbits.com/img/a1131746092_10.jpg"
+		},
+		{
+			name: "TypeScript",
+			image: "https://raw.githubusercontent.com/remojansen/logo.ts/master/ts.png"
+		},
+		{
+			name: "Brainfuck",
+			image: "https://i.ytimg.com/vi/w9Hm53rJxYo/maxresdefault.jpg"
+		},
+		{
+			name: "WhiteSpace",
+			image: "http://whitespacers.com/images/assets/icons/ui-facebook-share.png"
+		},
+		{
+			name: "C",
+			image: "https://cdn.dribbble.com/users/60166/screenshots/3829366/c_logo.jpg"
+		},
+		{
+			name: "C++",
+			image: "https://raw.githubusercontent.com/isocpp/logos/master/cpp_logo.png"
+		},
+		{
+			name: "C#",
+			image: "https://ih0.redbubble.net/image.416412087.0587/flat,550x550,075,f.jpg"
+		},
+		{
+			name: "Swift",
+			image:
+				"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Swift_logo.svg/1138px-Swift_logo.svg.png"
+		},
+		{
+			name: "Fortran",
+			image: "https://www.technotification.com/wp-content/uploads/2018/08/fortran-logo-300x115.png"
+		},
+		{
+			name: "PHP",
+			image: "https://cdn.worldvectorlogo.com/logos/php.svg"
+		},
+		{
+			name: "Haskell",
+			image: "https://wiki.haskell.org/wikiupload/0/03/HaskellLogoIdea05.gif"
 		}
 	];
 	techs.forEach(t => {
